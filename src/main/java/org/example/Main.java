1b40673f2a842a0072d6e83b593ab906a86f360d
Miguelokito13 <111703613+Miguelokito13@users.noreply.github.com>
2023-12-04 15:35:08 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Entradas del problema
+        String NombreUsuario="deyner";
+        short edadUsuario=34;
+        String telefonoUsuario="3234611668";
+        String documentoidentidad="1045486681";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotamarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotajunio;
+        int cuotajulio;
+        int cuotaagosto;
+        int cuotaseptiembre;
+        int cuotaoctubre;
+        int cuotanoviembre;
+        String direccionUsurio="crr 26 eg #37-160";
+        double costoinscripcion=15000.0;
+        float tasadeinteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor especifico a una variable
+        // desde el teclado (recibir datos)
+        Scanner leetDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        //leetDato.nextLine();
+    }
+}
